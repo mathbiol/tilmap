05ecb0d256e7747ac8ca103e5941e0779ae3fa02
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-10-25 15:02:47 -0400

    testing zoom

--- tammy.js
+++ tammy.js
@@ -7,14 +7,6 @@ tammy = function (event) {
     var clickPositionX = event.clientX;
     var clickPositionY = event.clientY;
     var ifrm = document.getElementById('caMicrocopeIfr');
-    var url = getUrl(clickPositionX, clickPositionY, ifrm);
-    console.log('URL:', url);
-    //Uncomment when ready
-    //ifrm.src = url;
-
-};
-
-function getUrl(clickPositionX, clickPositionY, ifrm) {
 
     // Domain could change in main program, so get existing url
     var loc = ifrm.src;
@@ -31,23 +23,23 @@ function getUrl(clickPositionX, clickPositionY, ifrm) {
     var imgWidth = img.width;
     var imgHeight = img.height;
 
-    // iFrame size
-    var frmWidth = ifrm.width;
-    var frmHeight = ifrm.height;
+    // Get slide dimensions
+    tammy.getFile('slidemeta.json').then(x => {
+        var tissue = tilmap.selTumorTissue.value.slice(0, -4);
+        var w_h = x[tissue];
+        var factor1 = w_h.width / imgWidth;
+        var factor2 = w_h.height / imgHeight;
+        var url = loc + "&x=" + Math.ceil(clickPositionX * factor1) + "&y=" + Math.ceil(clickPositionY * factor2);
+        console.log('URL:', url);
+        ifrm.src = url;
+        return url;
 
-    // Cross-Domain not allowed.
-    //console.log("scrollHeight", ifrm.contentWindow.document.body.scrollHeight);
+    });
 
-    var factor1 = frmWidth / imgWidth;
-    var factor2 = frmHeight / imgHeight;
+};
 
-    // Convert to viewport coordinates
-    var viewportPtX = clickPositionX * factor1;
-    var viewportPtY = clickPositionY * factor2;
-
-    // {'x': Math.round(xx), 'y': Math.round(yy)};
-
-    return loc + "&x=" + viewportPtX + "&y=" + viewportPtY;
-
-}
+tammy.getFile = async function (url) {
+    url = url || 'slidemeta.json';
+    return (await fetch(url)).json()
+};
 
