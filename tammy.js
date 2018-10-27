2b4b22d543b7f5d2a981edcb6ceb314b5ce35172
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2018-10-27 16:14:48 -0400

    fixed

--- tammy.js
+++ tammy.js
@@ -9,29 +9,46 @@ tammy = function (event) {
     // Get click position
     pos_x = event.offsetX ? (event.offsetX) : event.pageX - document.getElementById("imgTILDiv").offsetLeft;
     pos_y = event.offsetY ? (event.offsetY) : event.pageY - document.getElementById("imgTILDiv").offsetTop;
+    console.log("(pos_x, pos_y)", pos_x, pos_y);
 
-    // Calculate target coordinates
-    // Image Size
-    let img = document.getElementsByTagName('canvas')[0];
-    let imgWidth = img.width;
-    let imgHeight = img.height;
+    // Image Size is on the canvas
+    let canvases = document.getElementsByTagName("canvas");
+    let imgWidth;
+    let imgHeight;
+    for (let i = 0; i < canvases.length; i++) {
+        if (canvases[i].width > 0) {
+            imgWidth = canvases[i].width;
+            imgHeight = canvases[i].height;
+            console.log("(imgWidth, height)", imgWidth, imgHeight);
+            break;
+        }
+    }
 
     // Get slide dimensions
     tammy.getFile('slidemeta.json').then(x => {
-        let tissue = tilmap.selTumorTissue.value.slice(0, -4);
-        let w_h = x[tissue];
-        let factor1 = w_h.width / imgWidth;
-        let factor2 = w_h.height / imgHeight;
+        let slide = tilmap.selTumorTissue.value.slice(0, -4);
+        let slideDim = x[slide];
+        console.log("(slide width, height)", slideDim.width, slideDim.height);
+
         // Get current url
         let ifrm = document.getElementById('caMicrocopeIfr');
         let loc = ifrm.src;
-        // Strip location parameters (if exist)
-        if (loc.indexOf('&x=') > -1)
-            loc = loc.substring(0, loc.indexOf('&x='));
 
-        let url = loc + "&x=" + Math.ceil(pos_x * factor1) + "&y=" + Math.ceil(pos_y * factor2) + "&zoom=5";
-        console.log('URL:', url);
-        ifrm.src = url;
+        if (slideDim.width) {
+            let scaleWidth = slideDim.width / imgWidth;
+            let scaleHeight = slideDim.height / imgHeight;
+            // Strip existing parameters
+            if (loc.indexOf('&x=') > -1)
+                loc = loc.substring(0, loc.indexOf('&x='));
+            // Set url
+            let url = loc + "&x=" + Math.ceil(pos_x * scaleWidth) + "&y=" + Math.ceil(pos_y * scaleHeight) + "&zoom=5";
+            ifrm.src = url;
+            console.log('URL:', url);
+        }
+        else {
+            ifrm.src = loc; // Refresh.
+            console.log("*** CHECK SLIDE " + slide + "***");
+        }
 
     });
 
