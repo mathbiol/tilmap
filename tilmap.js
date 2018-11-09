5c9d20edd36436c898c39a275a5f290b4b341707
Jonas Almeida <jalmeida@mathbiol.org>
2018-11-09 11:03:04 -0500

    new pngs from Han

--- tilmap.js
+++ tilmap.js
@@ -100,7 +100,7 @@ tilmap.showTIL=function(){ // get image and display it
     //var h2 ='<h3>Interactive Analytics</h3>'
     var h2 =''
     var url2='https://quip1.bmi.stonybrook.edu:8443/camicroscope/osdCamicroscope.php?tissueId='+tilmap.selTumorTissue.value.replace('.png','')
-    if(tilmap.selTumorTissue.value.match(/\-/g).length<3){ // to accomodate Han's new slides
+    if(!tilmap.selTumorTissue.value.match('-')){ // to accomodate Han's new slides
         let id = tilmap.selTumorTissue.value.match(/\d+/)[0]
         url2="https://quip3.bmi.stonybrook.edu/camicroscope/osdCamicroscope.php?tissueId="+id
     }
