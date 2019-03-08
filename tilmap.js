e74444a24fac26618c88cb0e3a77de3e4ec69939
Jonas Almeida <jalmeida@mathbiol.org>
2019-03-08 10:07:53 -0500

    Update tilmap.js

--- tilmap.js
+++ tilmap.js
@@ -177,7 +177,7 @@ tilmap.showTIL=function(){ // get image and display it
     }
     else
     {
-        url2='http://quip1.bmi.stonybrook.edu:443/camicroscope/osdCamicroscope.php?tissueId='+tilmap.selTumorTissue.value.replace('.png','')
+        url2='https://quip1.bmi.stonybrook.edu/camicroscope/osdCamicroscope.php?tissueId='+tilmap.selTumorTissue.value.replace('.png','')
         if(!tilmap.selTumorTissue.value.match('-')){ // to accommodate Han's new slides
             let id = tilmap.selTumorTissue.value.match(/\d+/)[0]
             url2="http://quip3.bmi.stonybrook.edu/camicroscope/osdCamicroscope.php?tissueId="+id
